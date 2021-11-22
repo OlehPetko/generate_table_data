5141432fd0da09bf96f01945586d6cda2b1a7ef4
Hey OlehPetko! <petkooleg@gmail.com>
2021-11-22 22:34:48 +0000

    innit commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {nanoid} from "nanoid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialData = [
+        [{id: nanoid()}, {id: nanoid()}],
+        [{id: nanoid()}, {id: nanoid()}],
+        [{id: nanoid()}, {id: nanoid()}],
+        [{id: nanoid()}, {id: nanoid()}],
+    ]
+    const [columns, setColumns] = useState(1)
+    const [row, setRow] = useState(1)
+    const [tableData, setTableData] = useState(initialData)
+    const minusCol = () => {
+        if (columns > 1) {
+            setColumns(columns - 1)
+        }
+    }
+    const minusRow = () => {
+        if (row > 1) {
+            setRow(row - 1)
+        }
+    }
+    return (
+        <div className="App">
+            <button onClick={() => setColumns(columns + 1)}>plus columns</button>
+            {columns}
+            <button onClick={minusCol}>minus columns</button>
+            <hr/>
+            <button onClick={() => setRow(row + 1)}>plus row</button>
+            {row}
+            <button onClick={minusRow}>minus row</button>
+            <hr/>
+            <table border={1}>
+                <tbody>
+                {tableData.map((row, index) =>
+                <tr key={index}>
+                    {row.map(col =>
+                    <td key={col.id}>
+                        {col.id}
+                    </td>
+                    )}
+                </tr>
+                )}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
